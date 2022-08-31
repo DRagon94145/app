3aca3efa25d0ad7a2fd56a1b4af5ca7f8171ed2a
DRagon94145 <91694576+DRagon94145@users.noreply.github.com>
2022-08-31 18:42:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const { scrape } = require('./scraper')
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+
+app.get('/api/:bin', async (req, res) => {
+    let data = await scrape(req.params.bin);
+    res.set("Cache-Control", "public, max-age=86400");
+    res.type('application/json');
+    res.send(data);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
